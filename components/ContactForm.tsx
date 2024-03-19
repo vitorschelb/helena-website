913c74b9933b8daf05cf974499c0e9cc1086ba53
vitoraiala@gmail.com <vitoraiala@gmail.com>
2024-03-19 16:50:39 -0300

    FEA: Implement Zod and ReactHookForms on ContactForm

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -1,37 +1,57 @@
-export default function ContacForm() {
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+const contactFormSchema = z.object({
+  nome: z.string(),
+  email: z.string(),
+  mensagem: z.string(),
+})
+
+type ContactFormSchema = z.infer<typeof contactFormSchema>
+
+export default function ContactForm() {
+  const { register, handleSubmit } = useForm<ContactFormSchema>({
+    resolver: zodResolver(contactFormSchema),
+  })
+
+  function handleFormContact(data: ContactFormSchema) {
+    console.log(data)
+  }
+
   return (
-    <div className="flex flex-col">
+    <form onSubmit={handleSubmit(handleFormContact)} className="flex flex-col">
       <input
         tabIndex={0}
         aria-label="Nome"
-        name="Nome"
         type="text"
         id="nome"
         placeholder="Nome"
         required
         className="mb-3 bg-whiteIce p-3 text-sm tracking-wider shadow-sm"
+        {...register('nome')}
       />
       <input
         tabIndex={0}
         aria-label="E-mail"
-        name="email"
         type="email"
         id="email"
         placeholder="E-mail"
         required
         className="mb-3 bg-whiteIce p-3 text-sm tracking-wider shadow-sm"
+        {...register('email')}
       />
       <textarea
         tabIndex={0}
         aria-label="Mensagem"
-        name="mensagem"
         id="mensagem"
         rows={6}
         placeholder="Mensagem"
+        {...register('mensagem')}
       />
       <button tabIndex={0} aria-label="Enviar" type="submit">
         Enviar
       </button>
-    </div>
+    </form>
   )
 }
